1a403809663199d083a451ae866b1cd4a87fc570
张栋 <zd198666@sina.com>
2016-08-20 13:50:40 +0800

    新建TestView

---
+++ GitTest/GitTest/TestView.h
@@ -0,0 +1,13 @@
+//
+//  TestView.h
+//  GitTest
+//
+//  Created by Frank on 16/8/20.
+//  Copyright © 2016年 QYER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+
+@end
---
+++ GitTest/GitTest/TestView.m
@@ -0,0 +1,21 @@
+//
+//  TestView.m
+//  GitTest
+//
+//  Created by Frank on 16/8/20.
+//  Copyright © 2016年 QYER. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
